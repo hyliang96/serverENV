45b1035fda7b0bad16f01dc426eb2b94c3b2ce84
Haoyu Liang <hyliang96@163.com>
2019-01-23 00:37:01 +0800

    logout push

---
+++ app_config/.polipo_gen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p ${localENV}/log/polipo
+
+echo "
+proxyAddress = \"127.0.0.1\"
+proxyPort = 8124
+
+allowedClients = 127.0.0.1
+allowedPorts = 1-65535
+
+socksParentProxy = \"127.0.0.1:1080\"
+socksProxyType = socks5
+
+daemonise = true
+logSyslog = true
+
+logFile = $localENV/log/polipo/log
+pidFile = $localENV/log/polipo/pid
+" >  $(dirname ${0})/.polipo
+
+
