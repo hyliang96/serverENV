6c94649a60884567c8be608ce3e27e55c06856d0
hyliang96 <hyliang96@hotmail.com>
2019-08-01 00:25:00 +0800

    change serverENV/admin_tool/all.sh to support early exit during run `all`

--- admin_tool/all.sh
+++ admin_tool/all.sh
@@ -130,7 +130,84 @@ fi
 # IFS=$IFS_old
 
 # ---------------------------------------
+# watch -n 0.1 -t nvidia-smi
+# timeout 2 watch -n 0.1 -t "ls $dir/*.feedback | sort --version-sort"
 # 并行遍历个服务器发送命令
+
+# _send_a_server()
+# {
+    # local server=$1
+    # echo "rsync -aHhzP -e \"ssh -o 'StrictHostKeyChecking no'\"  $@ $server:$server_path "
+    # command rsync -aHhzP -e "ssh -o 'StrictHostKeyChecking no'" $@ $server:$server_path >> $dir/$server.feedback 2>&1
+    # # echo "rsync -aHhzP -e \"ssh -F $ssh_config\" $@ $server:$server_path "
+    # # command rsync -aHhzP -e "ssh -F $ssh_config" $@ $server:$server_path >> $dir/$server.feedback 2>&1
+# }
+
+# _ssh_a_server()
+# {
+    # local server=$1
+    # ssh -o 'StrictHostKeyChecking no' $server "$cmds" >> $dir/$server.feedback 2>&1
+    # # ssh -F $ssh_config $server "$cmds" >> $dir/$server.feedback 2>&1
+    # # ssh -F $ssh_config -o 'StrictHostKeyChecking no' $server "$cmds" >> $dir/$server.feedback 2>&1
+
+# }
+
+# for server in ${servers[@]}; do
+# {
+    # if ! [ "$no_prompt" = true ]; then
+        # echo "====== $server ======" >> $dir/$server.feedback
+    # fi
+    # if [ "$send" = "true" ]; then
+        # _send_a_server $server
+    # # command表示系统原版rsync命令
+    # else
+        # _ssh_a_server $server
+    # fi
+# } &
+# done
+
+
+# show_state()
+# {
+    # if [ -f $dir/finished ]; then
+        # finished="`cat $dir/finished | sort --version-sort `"
+        # finished="${finished//
+# / }"
+        # echo -ne "$finished\r"
+    # fi
+# }
+
+
+for server in ${servers[@]}; do
+    echo $server >> $dir/servers
+    echo -n "$server " >> $dir/unfinished_output
+done
+
+watch -n 1 -t "cat $dir/unfinished_output && ls $dir/*.feedback | sort --version-sort | xargs -I {} cat {}" &
+
+
+exit_func()
+{
+    # 杀死所有子进程
+    pkill -P $$
+    # 输出ssh返回的结果
+    ls $dir/*.feedback | sort --version-sort | xargs -I {} cat {}
+    # 删除临时文件夹
+    rm $dir -rf
+    # 退出程序
+    exit
+}
+
+function ctrl_c() {
+    # kill %%
+    exit_func
+}
+
+trap ctrl_c SIGINT
+
+
+
+
 for server in ${servers[@]}; do
 {
     if ! [ "$no_prompt" = true ]; then
@@ -147,11 +224,49 @@ for server in ${servers[@]}; do
         # ssh -F $ssh_config $server "$cmds" >> $dir/$server.feedback 2>&1
         # ssh -F $ssh_config -o 'StrictHostKeyChecking no' $server "$cmds" >> $dir/$server.feedback 2>&1
     fi
+    echo "$server" >> $dir/finished
+    unfinished="`sort $dir/servers $dir/finished | uniq -u`"
+    unfinished="${unfinished//
+/ }"
+    # if [ "$unfinished" = '' ]; then
+        # rm $dir/unfinished_output
+    # else
+    echo $unfinished > $dir/unfinished_output
+    # fi
+
+    # finished="`cat $dir/finished | sort --version-sort`"
+    # finished="${finished//
+# / }"
+    # for i in ($finished)
+    # echo $finished > $dir/finished_output
 } &
 done
+
+while true; do
+    if [ "`cat $dir/unfinished_output`" = '' ]; then
+        # pkill -P $$
+        # kill -INT $(pidof watch)
+        exit_func
+        # exit
+        # break
+    fi
+done
+
 wait
 
-# 输出ssh返回的结果
-ls $dir/*.feedback | sort --version-sort | xargs -I {} cat {}
-# 删除临时文件夹
-rm $dir -rf
+# exit_func
+
+# for i in {1..10}; do
+
+    # sleep 1
+# done
+
+# sleep 1
+# kill -9 `ps aux | grep -v grep | grep 'watch -n 0.1 -t' | awk '{print $2}'`
+# killall "asdasdasda"
+# wait
+
+
+
+
+
