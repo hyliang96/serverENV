50a8175346f2205ff04257de6d074ca9b509bada
hyliang96 <hyliang96@hotmail.com>
2019-09-27 14:40:38 +0800

    logout push sshost5 2019-09-27 14:40:38

--- script/fanqiang/v2ray_host/v2ray_host_alias.sh
+++ script/fanqiang/v2ray_host/v2ray_host_alias.sh
@@ -2,7 +2,7 @@
 
 v2ray()
 {
-    if [ "$1" = 'install' ] || [ "$1" = 'uninstall' ] || ( ! ( command -v v2ray > /dev/null ) ); then
+    if [ "$1" = 'install' ] || [ "$1" = 'uninstall' ] || [ "`command v2ray -h 2&>1`" = '' ]; then
         sudo su -c 'bash <(curl -s -L https://git.io/v2ray.sh)'
     else
         sudo v2ray $@
