47e3c510c14d2203b397c3eee655e51bfd84f124
hyliang96 <hyliang96@hotmail.com>
2019-09-27 16:21:18 +0800

    logout push sshost4 2019-09-27 16:21:18

--- script/fanqiang/v2ray_host/v2ray_host_alias.sh
+++ script/fanqiang/v2ray_host/v2ray_host_alias.sh
@@ -2,8 +2,14 @@
 
 v2ray()
 {
-    if [ "$1" = 'install' ] || [ "$1" = 'uninstall' ] || [ "`command v2ray -h 2>&1`" = '' ]; then
+    if [ "$1" = 'install' ] || [ "$1" = 'uninstall' ]; then
         sudo su -c 'bash <(curl -s -L https://git.io/v2ray.sh)'
+    elif [ "`command v2ray -h 2>&1 | grep 'command not found'`" != '' ]; then
+        local prompt='没有安装v2ray，是否继续安装？'
+        local answer=$(bash -c 'read  -n 1 -p "'"$prompt"'? [Y/N] " c; echo $c'); echo
+        if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+            sudo su -c 'bash <(curl -s -L https://git.io/v2ray.sh)'
+        fi
     else
         sudo v2ray $@
     fi
