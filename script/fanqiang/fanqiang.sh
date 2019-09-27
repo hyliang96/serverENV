959a72c558606f9674e14229a52767f270f6f55c
hyliang96 <hyliang96@hotmail.com>
2019-09-27 12:55:19 +0800

    logout push sshost5 2019-09-27 12:55:19

---
+++ script/fanqiang/fanqiang.sh
@@ -0,0 +1,8 @@
+# get absoltae path to the dir this is in, work in bash, zsh
+# if you want transfer symbolic link to true path, just change `pwd` to `pwd -P`
+here=$(cd "$(dirname "${BASH_SOURCE[0]-$0}")"; pwd)
+
+. $here/shadowsocks_client/ss_client_alias.sh
+. $here/shadowsocks_host/ss_host_alias.sh
+
+unset -a here
