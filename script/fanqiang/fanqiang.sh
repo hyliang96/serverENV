5def1f8abae68457cfc0b92737998f4ff9ef8841
hyliang96 <hyliang96@hotmail.com>
2019-09-27 12:59:07 +0800

    logout push sshost5 2019-09-27 12:59:07

--- script/fanqiang/fanqiang.sh
+++ script/fanqiang/fanqiang.sh
@@ -2,7 +2,7 @@
 # if you want transfer symbolic link to true path, just change `pwd` to `pwd -P`
 here=$(cd "$(dirname "${BASH_SOURCE[0]-$0}")"; pwd)
 
-. $here/shadowsocks_client/ss_client_alias.sh
-. $here/shadowsocks_host/ss_host_alias.sh
+. $here/shadowsocks_client/ss_client_alias.sh || echo "$here/shadowsocks_client/ss_client_alias.sh not found!"
+. $here/shadowsocks_host/ss_host_alias.sh || echo "$here/shadowsocks_host/ss_host_alias.sh not found!"
 
 unset -a here
