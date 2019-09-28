ec9c6fd70143c63f155be1551e578a08079798c3
hyliang96 <hyliang96@hotmail.com>
2019-09-29 03:48:09 +0800

    logout push jungpu1 2019-09-29 03:48:09

--- script/fanqiang/fanqiang.sh
+++ script/fanqiang/fanqiang.sh
@@ -2,8 +2,8 @@
 # if you want transfer symbolic link to true path, just change `pwd` to `pwd -P`
 here=$(cd "$(dirname "${BASH_SOURCE[0]-$0}")"; pwd)
 
-. $here/shadowsocks_client/ss_client_alias.sh
-. $here/shadowsocks_host/ss_host_alias.sh
-. $here/v2ray_host/v2ray_host_alias.sh
+. $here/shadowsocks_client/ss_client_alias.sh  >&2
+. $here/shadowsocks_host/ss_host_alias.sh  >&2
+. $here/v2ray_host/v2ray_host_alias.sh  >&2
 
 unset -v here
